edfebbd89ea54d2454424a40249c96039bee0144
student05 <student05@corp.devexpress.com>
2017-07-20 14:18:27 +0300

    Add project

---
+++ Assets/Resources/Scripts/Base/Environment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Environment {
+
+    public static GameObject GetBreakWall()
+    {
+        return Resources.Load("Prefabs/Environment/BreakWall") as GameObject;
+    }
+
+    public static GameObject GetConcretWall()
+    {
+        return Resources.Load("Prefabs/Environment/ConcretWall") as GameObject;
+    }
+
+    public static GameObject GetGround()
+    {
+        return Resources.Load("Prefabs/Environment/Ground") as GameObject;
+    }
+
+}
---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	void Start () {
+        GenerateMap map = new GenerateMap();
+        map.CreateMap();
+    }
+
+}
